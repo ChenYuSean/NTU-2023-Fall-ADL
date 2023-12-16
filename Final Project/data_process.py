04b0e9019e898bf20761307aa49bc83d34448ec6
ChenYuSean <r12922148@csie.ntu.edu.tw>
2023-12-16 14:03:45 +0800

    Push: 1216

--- Final Project/data_process.py
+++ Final Project/data_process.py
@@ -15,24 +15,32 @@ import argparse
 
 
 # %%
-def read_json_files(path, max_file_num=None):
+def read_json_files(path, max_file_num:int = None, shuffle = True):
     json_files_content = {}
     content_list = []
-    file_num = 0
-    for root, dirs, files in os.walk(path):
-        for file in files:
+    # Get the file names
+    files = []
+    for root, dirs, filenames in os.walk(path):
+        for file in filenames:
             if file.endswith('.json') and file != "star_record.json":
-                file_num += 1
-                with open(os.path.join(root, file), 'r', encoding='utf-8') as f:
-                    content = json.load(f)
-                    # json_files_content[file] = content
-                    for record in content:
-                        if type(record['votes']) == str:
-                            if '萬' in record['votes']:
-                                record['votes'] = int(record['votes'].replace('萬', '')) * 10000
-                    content_list += content
-            if max_file_num is not None and file_num >= max_file_num:
-                break
+                files.append(os.path.join(root, file))  
+    # Shuffle the list
+    if shuffle:
+        random.shuffle(files)
+    # Read the files
+    file_num = 0         
+    for file in files:
+        file_num = file_num + 1
+        with open(file, 'r', encoding='utf-8') as f:
+            content = json.load(f)
+            # json_files_content[file] = content
+            for record in content:
+                if type(record['votes']) == str:
+                    if '萬' in record['votes']:
+                        record['votes'] = int(record['votes'].replace('萬', '')) * 10000
+            content_list += content
+        if max_file_num is not None and file_num >= max_file_num:
+            break
     return content_list
 
 
@@ -54,22 +62,23 @@ def get_prompt(title:str, description:str, star_num:str, mood:str) -> str:
         mood = '生氣'
     elif mood == 'surprise':
         mood = '驚訝'
+    elif mood == 'fear':
+        mood = '害怕'
     elif mood == 'none':
         mood = '中立'
     
-    return f"{seed} 你是人工智慧助理，以下是用戶和人工智能助理之間的對話。你要對用戶的問題提供有用、安全、詳細和禮貌的回答。\
+    return f"{seed} 你是人工智慧助理，以下是用戶和人工智能助理之間的對話。你要對用戶的問題提供有用、詳細的回答。\
 USER: 請幫這部影片生出對應需求的{comment_type}。影片標題:[{title}]。影片敘述:[{description}]。需求情感:[{mood}]。\
 ASSISTANT:"
 
 
 # %%
-# %%
 if __name__ == '__main__':
-    PATH = "./train_data"
-    content = read_json_files(PATH,500)
+    PATH = "./train_data/"
+    content = read_json_files(PATH, 300, True)
+    print(content[:5])
     dataset = Dataset.from_list(content)
     print(dataset)
-    
 
 
 
